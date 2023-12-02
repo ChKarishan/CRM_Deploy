1e4d9656afe6ff04c584532b60bfe162f2834ebf
ChKarishan <karishan.897@gmail.com>
2023-12-02 19:21:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.route('/test',async (req, res) => {
+
+    res.json("test api ");
+})
+
+app.listen(8888, () => console.log(`Server Port: 8888`));
